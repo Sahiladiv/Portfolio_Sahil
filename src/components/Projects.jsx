8561eea181c95a8f8c78a1402bfd60bfdbf511fc
Sahil Adivarekar <63301456+Sahiladiv@users.noreply.github.com>
2025-01-15 18:37:57 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,6 +17,7 @@ const Projects = () => {
             {/* Card Content */}
             <div className="p-6 text-neutral-800">
               <h3 className="text-xl font-semibold mb-2">Context-Awareness Neurosymbolic Image Augmentation</h3>
+              <div className="w-full h-[1px] bg-gray-300 mb-4"></div> {/* Faint grey line */}
               <p className="mb-4">
                 Developed a novel augmentation framework merging symbolic rules and multi-modal representations (CLIP, FILIP, BART) for realistic, context-aware composite images.
               </p>
@@ -27,6 +28,7 @@ const Projects = () => {
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="p-6 text-neutral-800">
               <h3 className="text-xl font-semibold mb-2">Feature Mixing with Gradient Descent for Plant Disease Detection</h3>
+              <div className="w-full h-[1px] bg-gray-300 mb-4"></div>
               <p className="mb-4">
                 Created a feature-based augmentation technique leveraging the Hadamard matrix and SGD to boost model performance in detecting plant diseases using architectures like ResNet-50, VGG-16, and VGG-19.
               </p>
@@ -37,6 +39,7 @@ const Projects = () => {
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="p-6 text-neutral-800">
               <h3 className="text-xl font-semibold mb-2">ASL Recognition with Computer Vision and Deep Learning</h3>
+              <div className="w-full h-[1px] bg-gray-300 mb-4"></div>
               <p className="mb-4">
                 Implemented a pipeline using SIFT, ORB, FLANN approach to detect and classify American Sign Language alphabets and phrases.
               </p>
@@ -47,6 +50,7 @@ const Projects = () => {
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="p-6 text-neutral-800">
               <h3 className="text-xl font-semibold mb-2">Old Photo Restoration using Auto-encoders</h3>
+              <div className="w-full h-[1px] bg-gray-300 mb-4"></div>
               <p className="mb-4">
                 Developed an auto-encoder method to remove noise and scratches from vintage photos, enhancing color and resolution for improved image quality.
               </p>
@@ -57,6 +61,7 @@ const Projects = () => {
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="p-6 text-neutral-800">
               <h3 className="text-xl font-semibold mb-2">Inkspire</h3>
+              <div className="w-full h-[1px] bg-gray-300 mb-4"></div>
               <p className="mb-4">
                 Built a Django-based blogging platform featuring a recommendation engine and NLP functionalities (text summarization, paraphrasing, spell correction, sentiment analysis) for seamless blog creation and sharing.
               </p>
@@ -67,6 +72,7 @@ const Projects = () => {
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="p-6 text-neutral-800">
               <h3 className="text-xl font-semibold mb-2">Data Analysis of Rainfall Patterns in Mumbai</h3>
+              <div className="w-full h-[1px] bg-gray-300 mb-4"></div>
               <p className="mb-4">
                 This project explored rainfall patterns in Mumbai, India, through a collaboration with the Maharashtra Meteorological Department. By diving deep into weather data, we conducted exploratory data analysis (EDA), feature engineering, and data manipulation to uncover meaningful trends and patterns.
               </p>

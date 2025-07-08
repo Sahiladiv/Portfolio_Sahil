dd9a33a4840477dbc8b72bff0740e4db34ede638
Sahil Adivarekar <63301456+Sahiladiv@users.noreply.github.com>
2025-07-08 01:14:17 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,4 @@
-// import React from 'react'
-// import About from './components/About'
-// import Technology from './components/Technology'
-// import Experience from './components/Experience'
-// import Projects from './components/Projects'
-// import Contact from './components/Contact'
 
-// const App = () => {
-//   return (
-//     <div>
-//       <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-purple-300 selection:text-purple-900"></div>
-//       <div className="fixed top-0 -z-10 h-full w-full"></div>
-//       <div className="relative h-screen">
-//         {/* Background Pattern */}
-//         <div className="absolute inset-0">
-//           <div className="relative h-full w-full [&>div]:absolute [&>div]:bottom-0 [&>div]:right-0 [&>div]:z-[-2] [&>div]:h-full [&>div]:w-full [&>div]:bg-gradient-to-b [&>div]:from-blue-200 [&>div]:to-white">
-//             <div></div>
-//           </div>
-//         </div>
-//         <div className="container mx-auto px-8"></div>
-//         <About />
-//         <Technology />
-//         <Experience />
-//         <Projects />
-//         <Contact />
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
 
 
 import React from 'react'
@@ -38,7 +8,7 @@ import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Publications from './components/Publications'
 import Contact from './components/Contact'
-
+import Education from './components/Education'
 const App = () => {
   return (
     <div>
@@ -53,6 +23,7 @@ const App = () => {
         </div>
         <div className="container mx-auto"></div>
         <About />
+        <Education/>
         <Technology />
         <Experience />
         <Projects />

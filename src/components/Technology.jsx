eccde1ddffa2fca01916899ba00a93ad8ee5c166
Sahil Adivarekar <63301456+Sahiladiv@users.noreply.github.com>
2025-07-08 00:09:01 -0400

    Update Technology.jsx

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -1,84 +1,67 @@
 import React from 'react';
-import {
-  BiLogoPython,
-  BiLogoPostgresql,
-} from 'react-icons/bi';
-import { PiFileSqlFill } from 'react-icons/pi';
-import {
-  FaJava,
-  FaAws,
-  FaAndroid,
-  FaDocker,
-} from 'react-icons/fa';
-import {
-  SiDjango,
-  SiPytorch,
-  SiFastapi,
-  SiFlask,
-  SiScikitlearn,
-  SiTensorflow,
-  SiKubernetes,
-  SiNumpy,
-  SiPandas,
-  SiTensorboard,
-  SiOpenai,
-} from 'react-icons/si';
-import { IoLogoFirebase } from 'react-icons/io5';
+import { BiLogoPostgresql } from 'react-icons/bi'
+import { BiLogoPython } from "react-icons/bi";
+import { PiFileSqlFill } from "react-icons/pi";
+import { FaJava } from "react-icons/fa";
+import { FaAws } from "react-icons/fa";
+import { SiDjango } from "react-icons/si";
+import { SiPytorch } from "react-icons/si";
+import { FaAndroid } from "react-icons/fa";
+import { IoLogoFirebase } from "react-icons/io5";
 
-/* ------------------------------------------------------------------ */
-/*  Feel free to rearrange the list to change the display order.      */
-/* ------------------------------------------------------------------ */
-const tech = [
-  { name: 'Python', icon: <BiLogoPython className="text-7xl text-yellow-400" /> },
-  { name: 'PyTorch', icon: <SiPytorch className="text-7xl text-orange-700" /> },
-  { name: 'Hugging Face', img: '/assets/hugging-face.png' },
-  { name: 'TensorFlow', icon: <SiTensorflow className="text-7xl text-orange-600" /> },
-  { name: 'Scikit-learn', icon: <SiScikitlearn className="text-7xl text-orange-500" /> },
-  { name: 'LangChain', img: '/assets/langchain.png' },
-  { name: 'ChromaDB', img: '/assets/chromadb.png' },
-  { name: 'FastAPI', icon: <SiFastapi className="text-7xl text-emerald-600" /> },
-  { name: 'Flask', icon: <SiFlask className="text-7xl text-gray-600" /> },
-  { name: 'Django', icon: <SiDjango className="text-7xl text-green-700" /> },
-  { name: 'Java', icon: <FaJava className="text-7xl text-orange-400" /> },
-  { name: 'Android', icon: <FaAndroid className="text-7xl text-green-400" /> },
-  { name: 'SQL', icon: <PiFileSqlFill className="text-7xl text-red-400" /> },
-  { name: 'PostgreSQL', icon: <BiLogoPostgresql className="text-7xl text-sky-700" /> },
-  { name: 'Pandas', icon: <SiPandas className="text-7xl text-black" /> },
-  { name: 'NumPy', icon: <SiNumpy className="text-7xl text-indigo-600" /> },
-  { name: 'TensorBoard', icon: <SiTensorboard className="text-7xl text-orange-500" /> },
-  { name: 'OpenAI', icon: <SiOpenai className="text-7xl text-gray-900" /> },
-  { name: 'Docker', icon: <FaDocker className="text-7xl text-sky-500" /> },
-  { name: 'Kubernetes', icon: <SiKubernetes className="text-7xl text-blue-500" /> },
-  { name: 'Firebase', icon: <IoLogoFirebase className="text-7xl text-yellow-500" /> },
-  { name: 'AWS', icon: <FaAws className="text-7xl text-black" /> },
-];
+
+
 
 const Technology = () => {
   return (
-    <section id="technologies" className="my-20">
-      <h2 className="mb-8 text-center font-serif text-4xl text-black">
-        Technologies
-      </h2>
-
+    <div>
+      <h2 className="my-20 text-4xl font-serif text-black text-center mb-8">Technologies</h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
-        {tech.map(({ name, icon, img }) => (
-          <div
-            key={name}
-            className="flex flex-col items-center rounded-2xl border-4 border-gray-200 p-4"
-          >
-            {icon ??
-              (img && (
-                <img
-                  src={img}
-                  alt={name}
-                  className="h-20 w-20 object-contain"
-                />
-              ))}
-            <p className="mt-2 text-center text-black">{name}</p>
-          </div>
-        ))}
+        <div className="rounded-2xl border-4 border-white-800 p-4">
+          <BiLogoPython className="text-7xl text-yellow-400" />
+          <p className="mt-2 text-center">Python</p>
+        </div>
+        <div className="rounded-2xl border-4 border-white-800 p-4">
+          <SiPytorch className="text-7xl text-orange-700" />
+          <p className="mt-2 text-center">Pytorch</p>
+        </div>
+        <div className="rounded-2xl border-4 border-white-800 p-4">
+        <img
+          src="/assets/hugging-face.png"
+          alt="Hugging Face"
+          className="h-20 w-20 mx-auto"/>
+        <p className="mt-2 text-center">Hugging Face</p>
+        </div>
+        <div className="rounded-2xl border-4 border-white-800 p-4">
+          <SiDjango className="text-7xl text-green-700" />
+          <p className="mt-2 text-center">Django</p>
+        </div>
+        <div className="rounded-2xl border-4 border-white-800 p-4">
+          <FaJava className="text-7xl text-orange-400" />
+          <p className="mt-2 text-center">Java</p>
+        </div>
+        <div className="rounded-2xl border-4 border-white-800 p-4">
+          <FaAndroid className="text-7xl text-green-400" />
+          <p className="mt-2 text-center">Android</p>
+        </div>
+        <div className="rounded-2xl border-4 border-white-800 p-4">
+          <PiFileSqlFill className="text-7xl text-red-400" />
+          <p className="mt-2 text-center">SQL</p>
+        </div>
+        <div className="rounded-2xl border-4 border-white-800 p-4">
+          <BiLogoPostgresql className="text-7xl text-sky-700" />
+          <p className="mt-2 text-center">Postgresql</p>
+        </div>
+        <div className="flex flex-col items-center rounded-2xl border-4 border-gray-200 p-4">
+            <IoLogoFirebase className="text-7xl text-yellow-500" />
+            <p className="mt-2 text-center text-black">Firebase</p>
+        </div>
+        <div className="rounded-2xl border-4 border-white-800 p-4">
+          <FaAws className="text-7xl text-black-400" />
+          <p className="mt-2 text-center">AWS</p>
+        </div>        
       </div>
-    </section>
+    </div>
   );
 };
 
